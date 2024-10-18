087545812eb3d2a1f3b132d81d3f7ee537299fca
Marek Zeman <marekzeman91@gmail.com>
2024-10-18 18:50:28 +0200

    Lint fixes

--- misc/dist/html/service-worker.js
+++ misc/dist/html/service-worker.js
@@ -154,7 +154,7 @@ self.addEventListener('message', (event) => {
 		if (msg === 'claim') {
 			self.skipWaiting().then(() => self.clients.claim());
 		} else if (msg === 'clear') {
-			void caches.delete(CACHE_NAME);
+			caches.delete(CACHE_NAME);
 		} else if (msg === 'update') {
 			self.skipWaiting().then(() => self.clients.claim()).then(() => self.clients.matchAll()).then((all) => all.forEach((c) => c.navigate(c.url)));
 		}
