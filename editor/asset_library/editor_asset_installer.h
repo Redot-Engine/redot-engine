446a8b929c54e364d3d93622ee6e2b12dc7f837a
CharlieDoesStuff <charliephilp@outlook.com>
2025-12-23 13:30:33 +0000

    Add forward declaration for UnityAsset in editor_asset_installer.h

--- editor/asset_library/editor_asset_installer.h
+++ editor/asset_library/editor_asset_installer.h
@@ -34,7 +34,9 @@
 
 #include "scene/gui/dialogs.h"
 #include "scene/gui/tree.h"
-#include "editor/plugins/unity_package_importer.h"
+
+// Forward declarations
+struct UnityAsset;
 
 class CheckBox;
 class EditorFileDialog;
