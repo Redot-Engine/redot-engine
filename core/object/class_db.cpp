9ee4d6154455a494265b25b2b51f76dd3ce6ddf3
GusatuDamianAlexandru <damian.gusatu@stud.acs.upb.ro>
2026-02-05 22:44:38 +0200

    Core: Fix ClassDB class list sorting regression
    (cherry picked from commit 02a3ada50224e1cdc95077ee0d67d633ff904d7f)

--- core/object/class_db.cpp
+++ core/object/class_db.cpp
@@ -263,7 +263,7 @@ void ClassDB::get_class_list(LocalVector<StringName> &p_classes) {
 		p_classes.push_back(cls.key);
 	}
 
-	SortArray<StringName> sorter;
+	SortArray<StringName, StringName::AlphCompare> sorter;
 	sorter.sort(&p_classes[p_classes.size() - classes.size()], classes.size());
 }
 
@@ -287,7 +287,7 @@ void ClassDB::get_extensions_class_list(LocalVector<StringName> &p_classes) {
 		return;
 	}
 
-	SortArray<StringName> sorter;
+	SortArray<StringName, StringName::AlphCompare> sorter;
 	sorter.sort(&p_classes[original_size], p_classes.size() - original_size);
 }
 
