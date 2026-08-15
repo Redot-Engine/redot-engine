533fbab2da359a34b0619c7e429199835e804222
Francisco Miguel Almeida <francisco.m.almeida@gmail.com>
2026-08-15 20:21:24 +0200

    fix missing inf checks

--- modules/worldscape_3d/editor/worldscape_3d_editor.cpp
+++ modules/worldscape_3d/editor/worldscape_3d_editor.cpp
@@ -271,14 +271,14 @@ void WorldScape3DEditor::_operate_map(const Vector3 &p_global_position, const re
 			// Start brushing on the map
 			real_t brush_alpha = brush_image->get_pixelv(brush_pixel_position).r;
 			brush_alpha = real_t(Math::pow(double(brush_alpha), double(gamma)));
-			brush_alpha = std::isnan(brush_alpha) || std::isnan(brush_alpha) ? 0.f : CLAMP(brush_alpha, 0.f, 1.f);
+			brush_alpha = std::isnan(brush_alpha) || std::isinf(brush_alpha) ? 0.f : CLAMP(brush_alpha, 0.f, 1.f);
 			Color src = map->get_pixelv(map_pixel_position);
 			Color dest = src;
 
 			if (map_type == TYPE_HEIGHT) {
 				real_t srcf = src.r;
 				// In case data in existing map has nan or inf saved, check, and reset to real number if required.
-				srcf = std::isnan(srcf) || std::isnan(srcf) ? 0.f : srcf;
+				srcf = std::isnan(srcf) || std::isinf(srcf) ? 0.f : srcf;
 				real_t destf = srcf;
 
 				switch (_operation) {
