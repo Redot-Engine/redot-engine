65522906a62ad689237c61674055a7289867c752
JoltedJon <14169426+JoltedJon@users.noreply.github.com>
2026-09-01 19:34:38 -0400

    GH-1345 Simplify Vulkan check logic

--- modules/openxr/register_types.cpp
+++ modules/openxr/register_types.cpp
@@ -206,7 +206,7 @@ void initialize_openxr_module(ModuleInitializationLevel p_level) {
 #ifdef WINDOWS_ENABLED
 						"When using Windows Mixed Reality, note that WMR only has DirectX support. Make sure SteamVR is your default OpenXR runtime.\n"
 #endif
-						"Godot will start in normal mode.\n";
+						"Redot will start in normal mode.\n";
 
 				WARN_PRINT(init_error_message);
 
